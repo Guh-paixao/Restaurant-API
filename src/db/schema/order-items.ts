0c03e59aeaa864780275d7c0370ccf369761b6ab
Guh-paixao <gustavopaixao.dev@gmail.com>
2024-09-25 14:56:39 -0300

    Refactor foreign key constraints in schema files

--- src/db/schema/order-items.ts
+++ src/db/schema/order-items.ts
@@ -9,14 +9,14 @@ export const orderItems = pgTable("order_items", {
     orderId: text('order_id').notNull().references(() => orders.id, {
         onDelete: 'cascade',
     }),
-    productId: text('product_id').references(() => users.id, {
+    productId: text('product_id').references(() => products.id, {
         onDelete: 'set null',
     }),
     priceInCents: integer('price_in_cents').notNull(),
     quantity: integer('quantity').notNull(),
 });
 
-export const orderItemsRelations = relations(orderItems, ({ one, many }) => {
+export const orderItemsRelations = relations(orderItems, ({ one }) => {
     return {
         order: one(orders, {
             fields: [orderItems.orderId],
--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,18 +1,24 @@
 import { faker } from '@faker-js/faker';
-import { users, restaurants } from './schema';
+import { users, restaurants, orderItems, orders, products, authLinks } from './schema';
 import { db } from './connection';
 import chalk from 'chalk';
+import { createId } from '@paralleldrive/cuid2';
 
 
 //Reset Database
 await db.delete(users)
 await db.delete(restaurants)
+await db.delete(orderItems)
+await db.delete(orders)
+await db.delete(products)
+await db.delete(authLinks)
+
 
 console.log(chalk.yellow('✔️ Database Reset!'));
 
 
 //Create Customers
-await db.insert(users).values([
+const [customer1, customer2] = await db.insert(users).values([
     {
         name: faker.person.fullName(),
         email: faker.internet.email(),
@@ -23,7 +29,7 @@ await db.insert(users).values([
         email: faker.internet.email(),
         role: 'customer',
     },
-])
+]).returning()
 
 console.log(chalk.yellow('✔️ Created Customers!'));
 
@@ -43,15 +49,89 @@ console.log(chalk.yellow('✔️ Created Manager!'));
 
 
 //Create Restaurant
-await db.insert(restaurants).values([
+const [restaurant] = await db.insert(restaurants).values([
     {
         name: faker.company.name(),
         description: faker.lorem.paragraph(),
         managerId: manager.id
     }
-])
+]).returning()
 
 console.log(chalk.yellow('✔️ Created Manager!'));
+
+function generateProduct() {
+    return {
+        name: faker.commerce.productName(),
+        description: faker.commerce.productDescription(),
+        restaurantId: restaurant.id,
+        priceInCents: Number(faker.commerce.price({ min: 190, max: 490, dec: 0 })),
+    }
+}
+
+// Create Products
+const availableProducts = await db.insert(products).values([
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+    generateProduct(),
+]).returning()
+
+console.log(chalk.yellow('✔️ Created Products!'));
+
+
+// Create Orders
+
+type OrderItemsInsert = typeof orderItems.$inferInsert
+type OrdersInsert = typeof orders.$inferInsert
+
+const orderItemsToInsert: OrderItemsInsert[] = []
+const ordersToInsert: OrdersInsert[] = []
+
+for (let i = 0; i < 200; i++) {
+    const orderId = createId()
+
+    const orderProducts = faker.helpers.arrayElements(availableProducts, {
+        min: 1,
+        max: 3,
+    })
+
+    let totalInCents = 0
+
+    orderProducts.forEach(orderProducts => {
+        const quantity = faker.number.int({ min: 1, max: 5 })
+        totalInCents += orderProducts.priceInCents * quantity
+
+        orderItemsToInsert.push({
+            orderId,
+            productId: orderProducts.id,
+            priceInCents: orderProducts.priceInCents,
+            quantity,
+        })
+    })
+
+    ordersToInsert.push({
+        id: orderId,
+        customerId: faker.helpers.arrayElement([customer1.id, customer2.id]),
+        restaurantId: restaurant.id,
+        totalInCents,
+        status: faker.helpers.arrayElement([
+            'pending',
+            'processing',
+            'delivering',
+            'delivered',
+            'cancelled',
+        ]),
+        createdAt: faker.date.recent({ days: 40 })
+    })
+}
+
+await db.insert(orders).values(ordersToInsert)
+await db.insert(orderItems).values(orderItemsToInsert)
+
+console.log(chalk.yellow('✔️ Created Orders!'));
+
 
 console.log(chalk.greenBright('Seed applied successfully!'));
 
